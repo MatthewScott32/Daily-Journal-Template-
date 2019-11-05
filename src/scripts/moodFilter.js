e03eb936834fc0a6ee11861c4c240913655a39eb
Matthew Blagg <matthewscottblagg@gmail.com>
2019-11-05 14:57:23 -0600

    finished filter by mood

--- src/scripts/moodFilter.js
+++ src/scripts/moodFilter.js
@@ -1,28 +1,26 @@
 import API from "./data.js"
 import renderDom from "./entriesDOM.js"
 
-const moodContainer = {
- moodEvents()  {
-    const moodButtonArray = document.getElementsByName("moodButton")
-    moodButtonArray.forEach(function(button) {
-        button.addEventListener("click", event => {
-            API.getJournalEntries()
-            .then(entries => {
-              let emptyMoodArray = []
-              entries.forEach(function(entry){
-                  if(button.value === entry.mood){
-                  emptyMoodArray += entry
-                  }
-              })
-            renderDom.renderJournalEntries(emptyMoodArray)
-            })
-            
-            }
-            
-        )
-    })
- }
-}
+const moodContainer = {      //variable to export
+    moodEvents()  {          //overall function to invoke
+        const moodButtonArray = document.getElementsByName("moodButton") //variable containing the buttons
 
+         //forEach loop that runs through buttons and uses an empty function declaring button(taco)
+        moodButtonArray.forEach(function(button) {
+            button.addEventListener("click", event => {      //putseventlistener on buttons
+                API.getJournalEntries()                      //entries called
+                    .then(entries => {                       //entries(taco) function
+                        const filteredArray = entries.filter(entry =>   //takes the filtered array 
+                            button.value === entry.mood         //says button(taco) must equal mood
+                        )
+                        renderDom.renderJournalEntries(filteredArray)      //places results on dom
+                        })  
+                    })
+                        
+                })
+             }
+            }
+                        
+       
 
 export default moodContainer
